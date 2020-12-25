7280bd429e002107871f54eaaa5f399e81599d3b
thiagosprestes <thiago.santos.prestes@gmail.com>
2020-12-25 09:36:45 -0300

    Adicionada busca dinamica de posters no arquivo PostersController.

--- app/Controllers/Http/PostersController.ts
+++ app/Controllers/Http/PostersController.ts
@@ -3,6 +3,6 @@ import Application from '@ioc:Adonis/Core/Application'
 
 export default class PostersController {
   public async show({ params, response }: HttpContextContract) {
-    return response.download(Application.tmpPath(`/uploads/1608899126664_973877.jpg`))
+    return response.download(Application.tmpPath(`/uploads/${params.path}`))
   }
 }
